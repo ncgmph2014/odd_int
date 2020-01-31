bca88993aa37c51a0a3e5d01523be4563a1ae8b2
ncgmph2014 <nicole.gathany@gmail.com>
2020-01-30 23:25:50 -0500

    added examples arrays

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log("Testing. Testing. What's good, homies??");
+
+// Given an array, find the integer that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+
+function findOdd(A) {
+  //happy coding!
+  return 0;
+}
+
+findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]);
+findOdd([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]);
+findOdd([10]);
